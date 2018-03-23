85de9884f7e382d40641da26d24785694c1dd759
virtalas <virtalas@users.noreply.github.com>
2018-03-23 13:08:08 +0200

    comments for FileOutput

--- src/main/java/io/FileOutput.java
+++ src/main/java/io/FileOutput.java
@@ -7,12 +7,31 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 
+/**
+ * Class for reading bytes from the source file.
+ */
 public class FileOutput {
 
+    /**
+     * Used for writing bytes into the destination file.
+     */
     private OutputStream os;
+    
+    /**
+     * Keeps count for how many bits out of a byte are to be written.
+     */
     private int bitCount;
+    
+    /**
+     * Bits that are to be written are added to this byte.
+     * It is written when bitCount reaches eight.
+     */
     private int currentByte;
 
+    /**
+     * Initializes OutputStream, bitCount and currentByte.
+     * @param path path for the destination file.
+     */
     public FileOutput(String path) {
         try {
             os = new BufferedOutputStream(new FileOutputStream(new File(path)));
@@ -23,6 +42,10 @@ public class FileOutput {
         currentByte = 0;
     }
 
+    /**
+     * Write a string of bits to the destination file.
+     * @param bitsToWrite a string representation of the bits to be written
+     */
     public void writeBits(String bitsToWrite) {
         for (int i = 0; i < bitsToWrite.length(); i++) {
             char c = bitsToWrite.charAt(i);
@@ -31,23 +54,34 @@ public class FileOutput {
         }
     }
 
+    /**
+     * Private method for writing a single bit.
+     * @param bitToWrite bit to write as an integer that must be 0 or 1
+     */
     private void writeBit(int bitToWrite) {
         try {
             if (bitToWrite != 0 && bitToWrite != 1) {
-                throw new IllegalArgumentException("Argument must be 0 or 1");
+                throw new IllegalArgumentException("bitToWrite must be 0 or 1");
             }
-            currentByte = (currentByte << 1) | bitToWrite;
+            
+            currentByte = (currentByte << 1) | bitToWrite; // Shift bits to the left by one and place the new bit to the end.
             bitCount++;
+            
             if (bitCount == 8) {
+                // Full byte reached, write byte and start again.
                 os.write(currentByte);
                 currentByte = 0;
                 bitCount = 0;
             }
         } catch (IOException ex) {
-            System.out.println("Could not write to file.");
+            System.out.println("Could not write to file: " + ex);
         }
     }
 
+    /**
+     * Closes the OutputStream.
+     * If the current byte is not full, zeros are written to the end so the final byte can be written.
+     */
     public void close() {
         try {
             while (bitCount != 0) {
@@ -55,7 +89,7 @@ public class FileOutput {
             }
             os.close();
         } catch (IOException ex) {
-            System.out.println("Could not close OutputStream.");
+            System.out.println("Could not close OutputStream: " + ex);
         }
     }
 }
