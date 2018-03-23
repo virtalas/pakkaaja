d6dfd58a46dbfc3c82f7d7c262252514b37dd4a7
virtalas <virtalas@users.noreply.github.com>
2018-03-23 12:45:32 +0200

    comments for main

--- src/main/java/io/FileOutput.java
+++ src/main/java/io/FileOutput.java
@@ -6,8 +6,6 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public class FileOutput {
 
--- src/main/java/pakkaaja/Main.java
+++ src/main/java/pakkaaja/Main.java
@@ -2,24 +2,28 @@ package main.java.pakkaaja;
 
 import huffman.HuffmanCoder;
 import main.java.io.FileInput;
-import main.java.io.FileOutput;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.Map;
 
+/**
+ * Main class.
+ */
 public class Main {
 
+    /**
+     * Size of the array used to store character frequencies, or how many unique characters there are.
+     */
     public static final int ALPHABET_SIZE = 256;
 
     /**
-     * @param args the command line arguments are source, destination.
+     * Main program for compressing.
+     * A character is assumed to be one byte long. In the case it is not, the program will still work, just not as efficiently space saving wise.
+     * It reads the file and generates the character frequencies into a list that is passed on to the coder that will compress the file.
+     * @param args the command line arguments are: source, destination.
      */
     public static void main(String[] args) throws IOException {
+        // TODO: agruments for compression/decompression
         if (args.length != 2) {
-            System.out.println("Wrong number of arguments");
+            System.out.println("Usage: [source] [destination]");
             return;
         }
 
@@ -28,24 +32,20 @@ public class Main {
         int[] byteFrequencies = new int[ALPHABET_SIZE];
 
         FileInput in = new FileInput(sourcePath);
-
         int readByte = in.readNext();
 
         while (readByte != -1) {
-            byteFrequencies[readByte]++;
+            byteFrequencies[readByte]++; // Character as index, frequency count as value.
             readByte = in.readNext();
         }
 
         HuffmanCoder coder = new HuffmanCoder(byteFrequencies, ALPHABET_SIZE);
-
         coder.compress(sourcePath, destinationPath);
 
-        // Check the compressed content
+        // View some of the compressed content
         FileInput in2 = new FileInput(destinationPath);
-
         int readByte2 = in2.readNext();
-
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < 30; i++) {
             System.out.println(String.format("%8s", Integer.toBinaryString(readByte2 & 0xFF)).replace(' ', '0'));
             readByte2 = in2.readNext();
         }
